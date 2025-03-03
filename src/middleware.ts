8bb3f390f352fc1def1bf8daaed5164d93149eac
Nielleeeee <jandnllplz@gmail.com>
2025-03-04 05:24:08 +0800

    chore: updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth";
+import { PUBLIC_ROUTES } from "@/lib/constant";
 
 export async function middleware(request: NextRequest) {
   const sessionCookie = getSessionCookie(request);
+  const pathname = request.nextUrl.pathname;
+
+  if (
+    sessionCookie &&
+    PUBLIC_ROUTES.some((route) => pathname.startsWith(route))
+  ) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
 
   if (!sessionCookie) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
