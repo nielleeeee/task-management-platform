8976493ca51ab3a87ce9a0ce38a4f295a04fbbb4
Nielleeeee <jandnllplz@gmail.com>
2025-03-04 05:24:56 +0800

    feat: added react query

---
+++ src/lib/provider/provider.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useState } from "react";
+import { Header } from "@/components/layout/header";
+import { Toaster } from "@/components/ui/sonner";
+import { PUBLIC_ROUTES } from "@/lib/constant";
+import { usePathname } from "next/navigation";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const isPublicRoute = PUBLIC_ROUTES.some((route) =>
+    pathname.startsWith(route)
+  );
+
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 30 * 1000,
+            refetchInterval: 60 * 1000,
+          },
+        },
+      })
+  );
+
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Toaster />
+        {!isPublicRoute && <Header />}
+        {children}
+      </QueryClientProvider>
+    </>
+  );
+}
