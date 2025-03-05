9ad11aa2a61341adb97736951daee33bca19d75f
Nielleeeee <jandnllplz@gmail.com>
2025-03-06 00:53:37 +0800

    feat: created sign up page

---
+++ src/app/(pages)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import SignUpForm from "@/components/form/sign-in-form";
+
+export default function SignUp() {
+  return (
+    <main className="container mx-auto px-4">
+      <SignUpForm />
+    </main>
+  );
+}
