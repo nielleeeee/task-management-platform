1ce76e3273266a63445ef986a6faaa410df37217
Nielleeeee <jandnllplz@gmail.com>
2025-03-06 00:53:27 +0800

    feat: created sign in page

---
+++ src/app/(pages)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import SignInForm from "@/components/form/sign-in-form";
+
+export default function SignIn() {
+  return (
+    <main className="container mx-auto px-4">
+      <SignInForm />
+    </main>
+  );
+}
