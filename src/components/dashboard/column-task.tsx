3e3702de0c6f5ab291c56d50990fcf9ae4bc19eb
Nielleeeee <jandnllplz@gmail.com>
2025-03-22 02:49:54 +0800

    chore created column task

---
+++ src/components/dashboard/column-task.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { ColumnTaskProps } from "../../../types";
+import { useDroppable } from "@dnd-kit/core";
+import { TaskCard } from "@/components/dashboard/task-card";
+
+export function ColumnTask({
+  column,
+  tasks,
+  handleEdit,
+  handleDelete,
+}: ColumnTaskProps) {
+  const { setNodeRef } = useDroppable({
+    id: column.status,
+  });
+
+  return (
+    <div className="rounded-lg border flex flex-col min-h-80">
+      <div className="p-3 font-medium border-b bg-white">
+        <div className="flex justify-between items-center">
+          <h3 className="font-semibold">{column.title}</h3>
+        </div>
+      </div>
+
+      <div ref={setNodeRef} className="flex flex-col flex-grow p-4 gap-4">
+        {tasks.map((task) => (
+          <TaskCard
+            key={task.id}
+            task={task}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
